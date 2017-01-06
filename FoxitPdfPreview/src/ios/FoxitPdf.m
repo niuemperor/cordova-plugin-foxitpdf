ccaeb1f3eec5b90de81517f4b959e08fb0dd778b
huangn@sumilux.cn <huangn@sumilux.cn>
2017-01-06 10:34:04 +0800

    Integrated Foxit RDK UI and Functions

--- FoxitPdfPreview/src/ios/FoxitPdf.m
+++ FoxitPdfPreview/src/ios/FoxitPdf.m
@@ -4,13 +4,18 @@
 
 #import <FoxitRDK/FSPDFObjC.h>
 #import <FoxitRDK/FSPDFViewControl.h>
+#import "ReadFrame.h"
+#import "UIExtensionsSharedHeader.h"
 
-NSString *SN = @"ktoB0rBpK74y1CxIIa27WoLSBJEaSeR85eNAmzAk3UO+tLcp30i2Rw==";
-NSString *UNLOCK = @"ezKXjt3HtGhz9LvoL0WQjY537790VFfbykNCI0MicbT/nvzzLizkxROx+Qmgegu7fShWS6YDidw1+E87HZS9r6q4CfW0c5gwfCSQnzSBxfmvppgJ0zikaOXIOadxmE4JNny4oSkFRJ7zBxpXmOx1jL4zlaU/DIcJtrc0HspXwmrCsIFMhfTg5ZHdYHitxrhRdRsiHoVY8TYDE5hFpgO7GvHHEpGGCELwwM4x50Q/3416zJZOp8QMsriH+61WGuvthdHTk6B3YKB6djPM8USsoQR4RHmUC7bUCtOSB+zNyEhDq/kQurqLaQTjfjtW/5iBeQ2EdqUFokN8LGd805B/tgT9yABMK7U6z3HMfd3ytXVqddJLZEmaMNcDu3Z3bqK/T856kSwXcCLJqn10u9lfdmzR78OUSdfz4l2gyiSSJLcySxth/E0UZExXO3TmaTAnl0nxTp3dVwlBCI0CcEWs5q7RLOL75AEFnpJXBHFtEPHRL8rAF1weErmF8d4TCgbncfCO5bj2E3R6IsBzd3JvdrXGGkDvuZ3E3EdVdFeer1iv147XCx/x8PNcgctXwnQji88lFUypkP+YWtGQ1lBwLX2DrzsDe91L8Lp+4NIoc6nGoDuWasAGa6VbYpmxrYtvM+5b0eHotgnqrA7DcgCRI72lUHEzZCM/W0tKtQkspvGsu4XSgzHmbt0RqhKyGpBGS2poxR8Q0Kyj1C/ITz8Xswp2bxwf8HPL++skvZ3uxFyUevOLuTD+t5Jah81Nylefa/2vAA9zTn+FYNuJpv0W4uu4TPGg3qvEKWNR5fJpD2LhLsliaxg3S4HwX1NzmGeta91CgZdhKwO99HyZR/MzdJ2DbHXTeWr50jHWr5xSvcR30Uqg396JLbepvtXsEYtsJFymBJnmBwalykfmBWPAIAnWS95AUhOeu9OdoqllO84NLrlATHnhxf9mBP3eLsbKuQEPsQImHwCgOjkiR96Q3gmPqmupWo+O9XoFw7+aqprXNdjopR0j6kF43u839xWUKdb0Svz7/Hl/t2+bINuWu+vuseV7xvRKpe6oKFl/KS6WWapqU5s6WvIi6/3x2ZXFNW6PBn5jNn3reHFn4zqWu5k6MGTul8iMZII0QWRIzQb0sbkSnwncf6siZV45UUbiIpL3NMgMkjoNtWVhlO0Cu/UYTbbmpMjzIacfwcI+yuC/xUJwGq+jKh3ri0oV7q9Wz91P6Q==";
+
+NSString *SN = @"Q03VQoZzUrPy3RkJqM401mt4KTI2GoCaIX1z3VnXQ1a5NOJQWaVOWg==";
+NSString *UNLOCK = @"ezJvj93HvBh39LusL0W0ja4n+FSgXqp1ClndarfUeOP9wvCzbZdxF4ycpQufTTdu0MY9hJl6zUhrSykko0KyjhpaMFEn6IBy2Hiz7FezBuXN6xqYV1gE6OFxjeXHuiUMmVumuqq9TwbaUO3J7oa7wowS2vID86ofnSU5HyQwIZca5S7cqTnaRJEgzmo5rOWeb/UaqxDYhaEg4wnGLOqNAKk1f3byiS1jl84EJH33DLtf+sVZEuHtYSl8ES1YEni6IAbdkS6VelEdK7Pne4vn+e5FFkl0i4JrasjzbY3R34InVcID5w40lmDgMt6+YOd55tFhfLgJ4X+VLbYLNz6pTmO+GZdY5BwHfs8ZLNOd/xGWt7sRsoQt4PQ6KF+mrf91sCqKuGzEdjW+VHzWXwDhHot4dIA5xgZpRcdoqqDwC7huGvGv4W1cO1Xo1e1n+xcxNZ01uQ3hoE6iJBR1IhAUAhj90eZN2he9YX6UG3F9e6fJQeM8kWd/BxW6Oz9VE/k+EBQ6JKseWpbehuEZJ8VwocNpw1gt/U/WMOn93jt4NmMhbwP0PXD/7Ji1F4OMjFtx3wDu/zMvye286LvN1Vqt/KdMHs+xDvfy4SLr8bIn6emHXnWvzNtRBAJGVAi/A4XN4nTv+l+Nj+PdJy27SrbjUPI9b9brkizdWqbz+2sMDU9ln3LtfaXJloJRUTSli85YwGBXdyQaak888EQGMKsmOTmbgilrZ9CVuV7pIXUq1bsCCawuXzIxvCoQXjKTvVCHdC3MSzHDYd88vyHa9Qi2iGf0MPcXvQPw+CkaIsE3ziGbv8tTfo9Yjol+eaH9UCxPhtR3He8B/qIwpRoW6bTl79uTAo0vz+jiyzov9j2LH7fJZuwzx84UfsBoYiBMY4xR1usVPqPLoy4Ut49OP5lJYmzdrvKQc8v3PLmxWZk3t8Ga9C6wWCrEAdkqFKamWPlhSjXECNoVsVJWlpu7gyHD2ipAHP8879Q1z7z5Y1+ozmLirA+eUPBcl5l+GdR6DATSfZZGegMqbSDpzO+sghUYxCRAyeL0GHjs1kEPJpmcQFTAObh6+vQJyrTRULo+BAjhdFSvvaE17P9RTYzm72Nm0KD0o9PEdFHDvpCohXbRF/Uwj3l6/vti9kbXXQ8bQrCuN6lByCxidHhP+GcP7lQQCTmUGOP4pF/JqINV2BJ9Tk1UZGl2E33C8xDs0WDIQA==";
 
 @interface FoxitPdf : CDVPlugin {
-  // Member variables go here.
+    // Member variables go here.
 }
+
+@property (nonatomic, strong) ReadFrame* readFrame;
 
 - (void)Preview:(CDVInvokedUrlCommand*)command;
 @end
@@ -26,7 +31,7 @@ NSString *UNLOCK = @"ezKXjt3HtGhz9LvoL0WQjY537790VFfbykNCI0MicbT/nvzzLizkxROx+Qm
     
     // URL
     NSString *filePath = [command.arguments objectAtIndex:0];
-
+    
     // check file exist
     NSURL *fileURL = [[NSURL alloc] initWithString:filePath];
     BOOL isFileExist = [self isExistAtPath:fileURL.path];
@@ -39,7 +44,11 @@ NSString *UNLOCK = @"ezKXjt3HtGhz9LvoL0WQjY537790VFfbykNCI0MicbT/nvzzLizkxROx+Qm
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"preview success"];
         tmpCommandCallbackID = command.callbackId;
     } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageToErrorObject:1];
+        NSString* errMsg = [NSString stringWithFormat:@"file not find"];
+        UIAlertView* alert = [[[UIAlertView alloc] initWithTitle:@"Error" message:errMsg delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil] autorelease];
+        [alert show];
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR  messageAsString:@"file not found"];
     }
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
@@ -51,8 +60,13 @@ NSString *UNLOCK = @"ezKXjt3HtGhz9LvoL0WQjY537790VFfbykNCI0MicbT/nvzzLizkxROx+Qm
     // init foxit sdk
     enum FS_ERRORCODE eRet = [FSLibrary init:SN key:UNLOCK];
     if (e_errSuccess != eRet) {
+        NSString* errMsg = [NSString stringWithFormat:@"Invalid license"];
+        UIAlertView* alert = [[[UIAlertView alloc] initWithTitle:@"Check License" message:errMsg delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil] autorelease];
+        [alert show];
         return;
     }
+    
+    DEMO_APPDELEGATE.filePath = filePath;
     
     //load doc
     if (filePath == nil) {
@@ -66,33 +80,27 @@ NSString *UNLOCK = @"ezKXjt3HtGhz9LvoL0WQjY537790VFfbykNCI0MicbT/nvzzLizkxROx+Qm
     }
     
     //init PDFViewerCtrl
-    FSPDFViewCtrl* myTestViewCtrl;
-    myTestViewCtrl = [[FSPDFViewCtrl alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    [myTestViewCtrl setDoc:doc];
+    FSPDFViewCtrl* pdfViewCtrl;
+    pdfViewCtrl = [[FSPDFViewCtrl alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [pdfViewCtrl setDoc:doc];
     
-    UIViewController *testctrl = [[UIViewController alloc] init];
-    [testctrl.view addSubview:myTestViewCtrl];
-    testctrl.view.backgroundColor = [UIColor whiteColor];
+    self.readFrame = [[ReadFrame alloc] initWithPdfViewCtrl:pdfViewCtrl];
+    [pdfViewCtrl registerDocEventListener:self.readFrame];
     
-    //创建一个navigationbar
-    UINavigationBar *navBar = [[UINavigationBar alloc]initWithFrame:CGRectMake(0, 0, self.viewController.view.frame.size.width, 64)];
-    [testctrl.view addSubview:navBar];
-    //创建一个item
-    UINavigationItem *item = [[UINavigationItem alloc]initWithTitle:@"PDF Preview"];
-    navBar.items = @[item];
+    UIViewController *navCtr = [[UIViewController alloc] init];
+    [navCtr.view addSubview:pdfViewCtrl];
+    navCtr.view.backgroundColor = [UIColor whiteColor];
+    //    navCtr.modalPresentationStyle = UIModalPresentationFullScreen;
     
-    UIBarButtonItem *button = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(close)];
-    item.rightBarButtonItem = button;
     
-    [self.viewController presentViewController:testctrl animated:YES completion:nil];
+    self.readFrame.CordovaPluginViewController = navCtr;
+    
+    [self.viewController presentViewController:navCtr animated:YES completion:nil];
 }
 
 # pragma mark -- close preview
 -(void)close{
     [self.viewController dismissViewControllerAnimated:YES completion:nil];
-    
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"close success"];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:tmpCommandCallbackID];
 }
 
 # pragma mark -- isExistAtPath
